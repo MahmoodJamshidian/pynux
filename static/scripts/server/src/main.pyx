29b6dc5766e36c7b8889b72743ee62e627cebad1
MahmoodJamshidian <61678782+MahmoodJamshidian@users.noreply.github.com>
2023-01-12 18:40:34 +0330

    add comments

--- static/scripts/server/src/main.pyx
+++ static/scripts/server/src/main.pyx
@@ -14,63 +14,89 @@ import shlex
 
 
 cdef class Process:
+    """
+    process manager
+    """
     cdef int8_t exitcode
+    cdef str cmd
     def __init__(self, str cmd, on_write):
-        self.proc = Popen(shlex.split(cmd), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        # save on_write varable for sending output changes
         self.on_write = on_write
+        self.cmd = cmd
 
     def run(self):
+        """
+        output checker system
+        """
+        # io variable for saving STDOUT & STDERR and communicate output event listener with output getter
         io = [b''] * 2
 
         def return_writes(io):
-            res = b''
-            if io[0] != b'':
-                res += io[0]
-                io[0] = b''
-            if io[1] != b'':
-                res += io[1]
-                io[1] = b''
-            if res != b'':
+            """
+            output event detector
+            """
+            res = b'' # res variable to save the sum of STDOUT and STDERR
+            # save STDOUT value on res
+            res += io[0]
+            io[0] = b''
+            # save STDERR value on res
+            res += io[1]
+            io[1] = b''
+            if res != b'': # call on_write event if result is not empty
                 self.on_write(res)
 
         def read(io):
-            void_ret = [False] * 2
+            void_ret = [False] * 2 # void_ret for save STDOUT and STDERR is empty value for read full data
 
+            # read STDOUT and STDERR
             while True:
+                # iterate on readable outputs
                 for out in select.select([self.proc.stdout, self.proc.stderr], [], [], 0.01)[0]:
                     while True:
-                        d = out.read(1)
+                        loc_char = out.read(1) # read a character from output(STDOUT or STDERR)
+                        # check process status and check output value for read all data after process close
                         if self.proc.poll() != None:
                             if out == self.proc.stdout:
-                                if d == b'':
+                                if loc_char == b'':
                                     void_ret[0] = True
                             elif out == self.proc.stderr:
-                                if d == b'':
+                                if loc_char == b'':
                                     void_ret[1] = True
                             break
-
+                        # save STDOUT or STDERR data
                         if out == self.proc.stdout:
-                            io[0] += d
+                            io[0] += loc_char
                         elif out == self.proc.stderr:
-                            io[1] += d
+                            io[1] += loc_char
+                # break loop if process closed and outputs getted
                 if self.proc.poll() != None and void_ret[0] and void_ret[1]:
                     break
-
+        # run output getter system as a thread
         threading.Thread(target=read, args=(io,)).start()
 
-        res = b''
-        while self.proc.poll() == None:
+        # output event detector runner
+        while self.proc.poll() == None: # run output event detector, while process not closed
             return_writes(io)
-        return_writes(io)
+        return_writes(io) # run output event detector after process closed for return last data received
         
-        self.exitcode = self.proc.poll()
+        self.exitcode = self.proc.poll() # get process exit code
 
     def start(self):
-        threading.Thread(target=self.run).start()
+        """
+        start process and output checker
+        """
+        self.proc = Popen(shlex.split(self.cmd), stdin=PIPE, stdout=PIPE, stderr=PIPE) # open process
+        threading.Thread(target=self.run).start() # run output checker system
 
     def write(self, data):
+        """
+        write data to STDIN of process
+        """
         self.proc.stdin.write(data)
         self.proc.stdin.flush()
+
+class PyProc(Process):
+    pass
 
 
 app = Flask(__name__)
